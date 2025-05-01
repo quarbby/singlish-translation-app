5f6d61d1a5314b489ff0d5c45d22a21b4496a7a0
quarbby <lynnette-ng@outlook.com>
2025-05-01 10:01:48 -0400

    done files update

---
+++ get_done_files.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+df = pd.read_excel('Translations.xlsx', sheet_name="ALL_TRANSLATIONS", header=None)
+
+df['filename'] = df[3].apply(lambda x: x.replace('inputs/', ''))
+
+value_counts = df['filename'].value_counts()
+frequent_values = value_counts[value_counts >= 50].index.tolist()
+
+with open('done_files.txt', 'w') as f:
+    for fv in frequent_values:
+        f.write(fv + '\n')
+f.close()
